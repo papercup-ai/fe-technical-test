ee1690e6a1922ef5a2ee284782fb0a51630c62da
udoniyor <doniyor@papercup.com>
2020-12-01 10:50:10 +0000

    :memo: update task 6 to merge annotations

--- components/Tasks/task6.tsx
+++ components/Tasks/task6.tsx
@@ -4,11 +4,25 @@ import TaskBase from "./taskBase";
 const Task6 = () => {
   return (
     <TaskBase
-      title="Task 6: Detect if annotations overlap"
-      goal="Indicate if an annotation overlaps with another annotation(s)"
+      title="Task 6: Merge the annotations together if they overlap"
+      goal="Merge the annotations together if they overlap"
     >
-      Two or more annotations may overlap, and we would like to indicate an
-      overlap as an error on the annotation container (see Task 5).
+      Two or more annotations may overlap, and we would like to merge them into
+      one annotation and visualise that on the timeline we have created in the
+      previous tasks
+      <pre>
+        <br />
+        <br />
+        Example:
+        <br />
+        Input: intervals = [[12.9, 15.0],[14.2, 26.5],[27.2, 33.5]]
+        <br />
+        Output: [[12.9, 26.5],[27.2, 33.5]]
+        <br />
+        Explanation: Since intervals [12.9, 15.0], and [14.2, 26.5] overlaps,
+        merge them into [12.9, 26.5].
+        <br />
+      </pre>
     </TaskBase>
   );
 };
